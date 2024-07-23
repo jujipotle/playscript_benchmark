53b44a72b8fd2ffe2905e6746dfb800ea06c83bf
Justin Ji <justinji@college.harvard.edu>
2024-07-23 16:33:36 -0400

    Dataset of controlled ground-truth emotions

---
+++ playscript_utils.py
@@ -0,0 +1,9 @@
+model_name_dict = {
+    "llama2_13b_chat": "meta-llama/Llama-2-13b-chat-hf",
+    "llama3_8b": "meta-llama/Meta-Llama-3-8B",
+    "llama3_8b_instruct": "meta-llama/Meta-Llama-3-8B-Instruct",
+    "llama3_70b": "meta-llama/Meta-Llama-3-70B",
+    "llama3_70b_instruct": "meta-llama/Meta-Llama-3-70B-Instruct",
+    "test": "test"
+}
+
