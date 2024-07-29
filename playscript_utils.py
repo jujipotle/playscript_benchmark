9f6bfaf3b6adf258860bbcf0c396ba534571bd5d
Justin Ji <justinji@college.harvard.edu>
2024-07-28 23:20:57 -0400

    Logistic regression implementation and cleaning CSVs

--- playscript_utils.py
+++ playscript_utils.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 model_name_dict = {
     "llama2_13b_chat": "meta-llama/Llama-2-13b-chat-hf",
     "llama3_8b": "meta-llama/Meta-Llama-3-8B",
@@ -7,3 +9,11 @@ model_name_dict = {
     "test": "test"
 }
 
+emotions = ["happiness", "sadness", "anger", "fear", "disgust", "surprise"]
+
+def filter_csv_by_columns(csv_file_path, filter_criteria):
+    df = pd.read_csv(csv_file_path)
+    filtered_df = df
+    for column, value in filter_criteria.items():
+        filtered_df = filtered_df[filtered_df[column] == value]
+    return filtered_df
